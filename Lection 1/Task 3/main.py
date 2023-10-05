49a7366b326e3a94cb0510e27bc0c6152013a2e9
Anton <antonzaharchenko92@gmail.com>
2023-10-05 21:35:53 +0300

    Added third task of the first lection

---
+++ Lection 1/Task 3/main.py
@@ -0,0 +1,10 @@
+temp_scale = (input("What type of temperatue you want to choose Celsius(C) or Fahrenheit(F)"))
+temp = (float(input("Enter temperature")))
+if (temp_scale=="C"):
+    temp = (temp - 32)/ 1.8
+    print("convert value" , temp)
+elif (temp_scale=="F"):
+    temp = (temp * 1.8)/ 5 + 32
+    print ("covert value" , temp)
+else :
+    print("You write wrong values")
