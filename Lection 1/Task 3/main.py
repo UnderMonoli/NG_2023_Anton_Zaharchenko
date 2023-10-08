d062f40ac814d2e0e5fb5c95a88c2f0d27998f01
Anton <antonzaharchenko92@gmail.com>
2023-10-08 18:05:49 +0300

    Added third task of the first lection

--- Lection 1/Task 3/main.py
+++ Lection 1/Task 3/main.py
@@ -1,11 +1,11 @@
-temp_scale = (input("What type of temperatue you want to choose Celsius(C) or Fahrenheit(F)"))
-temp = (float(input("Enter your temperature")))
+temp_scale = (input("What type of temperatue you want to choose Celsius(C) or Fahrenheit(F) "))
+temp = (float(input("Enter your temperature ")))
 
-if (temp_scale=="C"):
-    temp = (temp - 32)/ 1.8
-    print("convert value" , temp)
-elif (temp_scale=="F"):
-    temp = (temp * 1.8)/ 5 + 32
-    print ("covert value" , temp)
+if (temp_scale == "C"):
+    converted_temp = (temp - 32)/ 1.8
+    print("Temperatur in Celsius: {:.2f}°C".format(converted_temp))
+elif (temp_scale == "F"):
+    converted_temp = (temp * 1.8)/ 5 + 32
+    print ("Temperatur in Fahrenheit: {:.2f}°F".format(converted_temp))
 else:
-    print("You write wrong values")
+    print("You write wrong values pls choose C of F")
