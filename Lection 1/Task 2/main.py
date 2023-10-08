0a541a1ec8d52c1dc70f398d363a9088bf2ca45a
Anton <antonzaharchenko92@gmail.com>
2023-10-08 17:34:16 +0300

    Added second task of the first lection

--- Lection 1/Task 2/main.py
+++ Lection 1/Task 2/main.py
@@ -1,4 +1,4 @@
-print("Enter Your Numbers")
-Your_First_Number=(int(input("First Number")))
-Your_Second_Number=(int(input("Second Number")))
-print("Sum= ", Your_First_Number + Your_Second_Number)
+print("Enter your numbers for the calculation of your sum")
+Your_First_Number = (int(input("First Number ")))
+Your_Second_Number = (int(input("Second Number ")))
+print("Sum=", Your_First_Number + Your_Second_Number)
