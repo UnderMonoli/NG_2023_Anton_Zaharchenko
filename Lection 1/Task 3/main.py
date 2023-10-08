87d1a10682c47bfe633ca37d5a07fd701742b0e8
Anton <antonzaharchenko92@gmail.com>
2023-10-08 17:48:48 +0300

    Added fourth task of the first lection

--- Lection 1/Task 3/main.py
+++ Lection 1/Task 3/main.py
@@ -1,10 +1,11 @@
 temp_scale = (input("What type of temperatue you want to choose Celsius(C) or Fahrenheit(F)"))
-temp = (float(input("Enter temperature")))
+temp = (float(input("Enter your temperature")))
+
 if (temp_scale=="C"):
     temp = (temp - 32)/ 1.8
     print("convert value" , temp)
 elif (temp_scale=="F"):
     temp = (temp * 1.8)/ 5 + 32
     print ("covert value" , temp)
-else :
+else:
     print("You write wrong values")
--- Lection 1/Task 4/main.py
+++ Lection 1/Task 4/main.py
@@ -2,8 +2,8 @@ cmd = "math_calculation"
 math_calculation=(input("which operation do you want? Plus(+) , Minus(-) , Division(/) , Multiplication(*) , Root(√) or Exponentiation(**)" ))
 a=(int(input("Number 1: ")))
 b=(int(input("Number 2: ")))
-match math_calculation:
 
+match math_calculation:
     case "+":
         print("c = " , a + b)
     case "-":
@@ -11,10 +11,15 @@ match math_calculation:
     case "*":
         print("c = " , a * b)
     case "/":
-        print("c = " , a / b)
+        if b != 0:
+            print("c = " , a / b)
+        else:
+            print("Error!!!!")
     case "**":
         print("c = " , a ** b)
     case "√":                               #To get this mathematical sign use Alt + 251
         print("c = " , a ** 0.5 )
+    case _:
+        print("This is not operation try again")
         
 print("c = " , math_calculation)
