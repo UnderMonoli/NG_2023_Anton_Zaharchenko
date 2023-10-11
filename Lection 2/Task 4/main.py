50000d0d4802045f9c49ac7ab998feedbbab8620
Anton <antonzaharchenko92@gmail.com>
2023-10-11 21:47:20 +0300

    Added fourth task for second lection

---
+++ Lection 2/Task 4/main.py
@@ -0,0 +1,16 @@
+list1 = []
+n = (int(input("Enter the list size ")))
+
+print("\n")
+for i in range(0, n):
+    print(f"Enter your word at index {i}:")
+    item = input()
+    if item.isalpha():
+        list1.append(item)
+    else:
+        print(f"'{item}' is not word and will be ignored.")
+
+for word in list1:
+    vowels = "aeiouAEIOU"
+    consonants_count = sum(1 for letter in word if letter.isalpha() and letter not in vowels)
+    print(f"Vomels in '{word}': {consonants_count}")
