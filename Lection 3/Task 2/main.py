747a7b56e8f74fd233dbe35f1e6837196b4675d7
Anton <antonzaharchenko92@gmail.com>
2023-10-20 17:34:08 +0300

    Added second task for third lection

---
+++ Lection 3/Task 2/main.py
@@ -0,0 +1,16 @@
+import platform
+
+laptop_info = {
+    "Name":"Lenovo Legion 5 15ACH6H",
+    "CPU":"Ryzen 5 5600H",
+    "RAM":"DDR4",
+    "Screen diagonal":"15.6",
+    "SDD":"Samsung",
+    "RamAmmount":"16Gb",
+    "GPU":"Radeon RX Vega 7",
+    "VideoCard":"Nvidia Geforce RTX 3070",
+    "OC":"Windows 10"
+}
+print("Laptop_info: ", "CPUdetails ")
+for key, value in laptop_info.items(), :
+    print(key + ":", value)
