c205b89653b36faaad5bd6572785028036faae17
Anton <antonzaharchenko92@gmail.com>
2023-11-03 17:59:45 +0200

    Returned for lection 2 Task 1

--- Lection 2/Task 1/main.py
+++ Lection 2/Task 1/main.py
@@ -2,7 +2,7 @@ number_list=[]
 n=(int(input("Enter the list size ")))
 
 print("\n")
-for i in range(0, n): 
+for i in range(0, 99999999999): 
     item = input(f"Enter your elements {i}: ")
     number_list.append(item)
 
