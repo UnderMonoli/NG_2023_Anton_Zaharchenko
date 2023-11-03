16a27856b6a31a5dbb610ba77b7bf131c0d536ed
Anton <antonzaharchenko92@gmail.com>
2023-11-03 17:53:01 +0200

    Retured for lection 4 task

--- Lection 1/Task 4/main.py
+++ Lection 1/Task 4/main.py
@@ -1,13 +1,13 @@
 cmd = "math_calculation"
 math_calculation=(input("which operation do you want? Plus(+) , Minus(-) , Division(/) , Multiplication(*) , Root(√) or Exponentiation(**)" ))
-Number_1=(int(input( )))        #(a) - number 1
-Number_2=(int(input( )))        #(b) - number 2
+Number_1=(int(input( )))       
+Number_2=(int(input( )))        
 
 result = None
 
 match math_calculation:
     case "+":
-        print("res = " , Number_1 + Number_2)       #(c) - result
+        print("res = " , Number_1 + Number_2)      
     case "-":
         print("res = " , Number_1 - Number_2)
     case "*":
@@ -19,7 +19,7 @@ match math_calculation:
             print("Error!!!!")
     case "**":
         print("res = " , Number_1 ** Number_2)
-    case "√":                               #To get this mathematical sign use Alt + 251
+    case "√":                                                   #To get this mathematical sign use Alt + 251
         print("res = " , Number_1 ** 0.5 )
     case _:
         print("This is not operation try again")
